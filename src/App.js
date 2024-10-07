acee06987c80d55c04f30ff053652490311ac7f9
MUGESH MANOHARAN <samcharlesmugesh@gmail.com>
2024-10-07 19:52:12 +0530

    .....

--- src/App.js
+++ src/App.js
@@ -8,12 +8,24 @@ import { AboutUs } from './pages/AboutUsPage/AboutUs';
 import ContactUs from './pages/ContactUsPage/ContactUs';
 import { Navbar } from './components/Navbar';
 import Footer from './components/Footer';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <ScrollToTop/>
       <Navbar/>
       <Routes>
         <Route path="/"  element={<Layout/>}>
